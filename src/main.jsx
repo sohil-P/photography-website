b18551db3e4891db1263baf743b353027fdb2140
sohil-P <sohilwork1343@gmail.com>
2025-06-02 19:56:57 +0530

    update file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Videography from "./pages/videography.jsx";
+import Videography from "./pages/Videography.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter basename="/photography-website/">
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/videography" element={<Videography />} />
+        <Route path="/Videography" element={<Videography />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
