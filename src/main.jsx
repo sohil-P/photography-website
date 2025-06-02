b939ccb61e49a31841778dffae36127fa7f60086
sohil-P <sohilwork1343@gmail.com>
2025-06-02 19:39:32 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/photography-website/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/videography" element={<Videography />} />
